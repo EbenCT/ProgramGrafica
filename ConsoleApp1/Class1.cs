75db494d12c550149e41f7e896f87b73648cd9b4
EbenCT <cayoeben64@gmail.com>
2024-03-22 19:45:58 -0300

    Primer commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,79 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public GameWindow windows;
+        private Television3D television3D;
+        private float rotationAngle = 0.0f;
+
+        public  Class1(GameWindow windowsInput)
+        {
+            this.windows= windowsInput;
+            television3D = new Television3D(); // Instancia de Television3D
+
+            windows.Load += windows_Load;
+            windows.RenderFrame += Windows_RenderFrame;
+            windows.UpdateFrame += Windows_UpdateFrame;
+            windows.Closing += Windows_Closing;
+            windows.KeyDown += Windows_KeyDown;
+        }
+
+        private void Windows_KeyDown(object sender, OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            // Rotar la vista en función de las teclas de flecha presionadas
+            switch (e.Key)
+            {
+                case OpenTK.Input.Key.Left:
+                    rotationAngle -= 5.0f; // Girar hacia la izquierda
+                    break;
+                case OpenTK.Input.Key.Right:
+                    rotationAngle += 5.0f; // Girar hacia la derecha
+                    break;
+            }
+        }
+        private void Windows_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Cerrar la aplicación
+            windows.Exit();
+        }
+
+        private void Windows_UpdateFrame(object sender, FrameEventArgs e)
+        {
+            
+        }
+
+        private void Windows_RenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Frustum(-1.0, 1.0, -1.0, 1.0, 1.0, 100.0); // Configura la proyección perspectiva
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            // Posiciona y orienta la cámara
+            GL.Translate(0.0f, 0.0f, -13.0f); // Mueve la cámara hacia atrás
+            GL.Rotate(-5.0f, 1.0f, 0.0f, 0.0f); // Inclina la vista hacia abajo
+            GL.Rotate(40.0f + rotationAngle, 0.0f, 1.0f, 0.0f); // Rota la vista hacia un ángulo lateral
+
+            television3D.Draw();
+            windows.SwapBuffers();
+        }
+
+        void windows_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.FromArgb(5, 5, 25));
+            GL.Enable(EnableCap.DepthTest); // Habilita el test de profundidad para objetos 3D
+
+        }
+    }
+}
---
+++ ConsoleApp1/Television3D.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+
+namespace ConsoleApp1
+{
+    internal class Television3D
+    {
+        public void Draw()
+        {
+            // Dibujar las líneas de los vértices en blanco
+            GL.Color3(1.0f, 1.0f, 1.0f); // Color blanco
+            GL.Begin(PrimitiveType.Lines);
+
+            // Líneas para la pantalla
+            GL.Vertex3(-5.5, -3, 0);
+            GL.Vertex3(5.5, -3, 0);
+
+            GL.Vertex3(5.5, -3, 0);
+            GL.Vertex3(5.5, 3.5, 0);
+
+            GL.Vertex3(5.5, 3.5, 0);
+            GL.Vertex3(-5.5, 3.5, 0);
+
+            GL.Vertex3(-5.5, 3.5, 0);
+            GL.Vertex3(-5.5, -3, 0);
+
+            // Líneas para los parlantes
+            GL.Vertex3(-5, -3.5, 0);
+            GL.Vertex3(-1, -3.5, 0);
+
+            GL.Vertex3(-1, -3.5, 0);
+            GL.Vertex3(-1, -4.5, 0);
+
+            GL.Vertex3(-1, -4.5, 0);
+            GL.Vertex3(-5, -4.5, 0);
+
+            GL.Vertex3(5, -3.5, 0);
+            GL.Vertex3(1, -3.5, 0);
+
+            GL.Vertex3(1, -3.5, 0);
+            GL.Vertex3(1, -4.5, 0);
+
+            GL.Vertex3(1, -4.5, 0);
+            GL.Vertex3(5, -4.5, 0);
+
+            // Líneas para los bordes
+            GL.Vertex3(-6, -5, 0);
+            GL.Vertex3(6, -5, 0);
+
+            GL.Vertex3(6, -5, 0);
+            GL.Vertex3(6, 4, 0);
+
+            GL.Vertex3(6, 4, 0);
+            GL.Vertex3(-6, 4, 0);
+
+            GL.Vertex3(-6, 4, 0);
+            GL.Vertex3(-6, -5, 0);
+
+            GL.Vertex3(-6, -5, 0);
+            GL.Vertex3(6, -5, 0);
+
+            GL.Vertex3(-6, -5, -1);
+            GL.Vertex3(6, -5, -1);
+
+            GL.Vertex3(6, -5, -1);
+            GL.Vertex3(6, 4, -1);
+
+            GL.Vertex3(6, 4, -1);
+            GL.Vertex3(-6, 4, -1);
+
+            GL.Vertex3(-6, 4, -1);
+            GL.Vertex3(-6, -5, -1);
+
+            // Líneas para la plataforma
+            GL.Vertex3(-1, -5, -0.75);
+            GL.Vertex3(1, -5, -0.75);
+
+            GL.Vertex3(1, -5, -0.75);
+            GL.Vertex3(1, -6, -0.75);
+
+            GL.Vertex3(1, -6, -0.75);
+            GL.Vertex3(-1, -6, -0.75);
+
+            GL.Vertex3(-1, -6, -0.75);
+            GL.Vertex3(-1, -5, -0.75);
+
+            GL.Vertex3(-1, -5, -0.25);
+            GL.Vertex3(1, -5, -0.25);
+
+            GL.Vertex3(1, -5, -0.25);
+            GL.Vertex3(1, -6, -0.25);
+
+            GL.Vertex3(1, -6, -0.25);
+            GL.Vertex3(-1, -6, -0.25);
+
+            GL.Vertex3(-1, -6, -0.25);
+            GL.Vertex3(-1, -5, -0.25);
+
+            GL.Vertex3(-1, -5, -0.25);
+            GL.Vertex3(-1, -5, -0.75);
+
+            GL.Vertex3(1, -5, -0.25);
+            GL.Vertex3(1, -5, -0.75);
+
+            GL.Vertex3(1, -6, -0.25);
+            GL.Vertex3(1, -6, -0.75);
+
+            GL.Vertex3(-1, -6, -0.25);
+            GL.Vertex3(-1, -6, -0.75);
+
+            // Líneas para la parte trasera
+            GL.Vertex3(-4, -6, 2);
+            GL.Vertex3(4, -6, 2);
+
+            GL.Vertex3(4, -6, 2);
+            GL.Vertex3(4, -6, -2);
+
+            GL.Vertex3(4, -6, -2);
+            GL.Vertex3(-4, -6, -2);
+
+            GL.Vertex3(-4, -6, -2);
+            GL.Vertex3(-4, -6, 2);
+
+            GL.End();
+
+            // Dibujar la televisión en 3D utilizando OpenGL
+            GL.Begin(PrimitiveType.Quads);
+
+            // Pantalla
+            GL.Color3(0.0f, 0.0f, 0.0f); // Color negro
+            GL.Vertex3(-5.5, -3, 0);
+            GL.Vertex3(5.5, -3, 0);
+            GL.Vertex3(5.5, 3.5, 0);
+            GL.Vertex3(-5.5, 3.5, 0);
+
+            //Parlantes
+            GL.Color3(0.0f, 0.0f, 0.0f); // Color negro
+            GL.Vertex3(-5, -3.5, 0);
+            GL.Vertex3(-1, -3.5, 0);
+            GL.Vertex3(-1, -4.5, 0);
+            GL.Vertex3(-5, -4.5, 0);
+
+            GL.Vertex3(5, -3.5, 0);
+            GL.Vertex3(1, -3.5, 0);
+            GL.Vertex3(1, -4.5, 0);
+            GL.Vertex3(5, -4.5, 0);
+
+            // Borde
+            GL.Color3(0.1f, 0.1f, 0.1f); // Color gris oscuro
+            GL.Vertex3(-6, -5, 0);
+            GL.Vertex3(6, -5, 0);
+            GL.Vertex3(6, 4, 0);
+            GL.Vertex3(-6, 4, 0);
+
+            GL.Vertex3(-6, -5, -1);
+            GL.Vertex3(6, -5, -1);
+            GL.Vertex3(6, 4, -1);
+            GL.Vertex3(-6, 4, -1);
+
+            GL.Vertex3(-6, -5, 0);
+            GL.Vertex3(6, -5, 0);
+            GL.Vertex3(6, -5, -1);
+            GL.Vertex3(-6, -5, -1);
+
+            GL.Vertex3(-6, 4, 0);
+            GL.Vertex3(6, 4, 0);
+            GL.Vertex3(6, 4, -1);
+            GL.Vertex3(-6, 4, -1);
+
+            GL.Vertex3(-6, -5, -1);
+            GL.Vertex3(-6, 4, -1);
+            GL.Vertex3(-6, 4, 0);
+            GL.Vertex3(-6, -5, 0);
+
+            GL.Vertex3(6, -5, -1);
+            GL.Vertex3(6, 4, -1);
+            GL.Vertex3(6, 4, 0);
+            GL.Vertex3(6, -5, 0);
+
+            // Plataforma
+            GL.Vertex3(-1, -5, -0.75);
+            GL.Vertex3(1, -5, -0.75);
+            GL.Vertex3(1, -6, -0.75);
+            GL.Vertex3(-1, -6, -0.75);
+
+            GL.Vertex3(-1, -5, -0.25);
+            GL.Vertex3(1, -5, -0.25);
+            GL.Vertex3(1, -6, -0.25);
+            GL.Vertex3(-1, -6, -0.25);
+
+            GL.Vertex3(-1, -5, -0.25);
+            GL.Vertex3(-1, -5, -0.75);
+            GL.Vertex3(1, -5, -0.25);
+            GL.Vertex3(1, -5, -0.75);
+
+            GL.Vertex3(-1, -6, -0.25);
+            GL.Vertex3(-1, -6, -0.75);
+            GL.Vertex3(1, -6, -0.25);
+            GL.Vertex3(1, -6, -0.75);
+
+            GL.Vertex3(-4, -6, 2); // Esquina inferior izquierda
+            GL.Vertex3(4, -6, 2); // Esquina inferior derecha
+            GL.Vertex3(4, -6, -2); // Esquina superior derecha
+            GL.Vertex3(-4, -6, -2); // Esquina superior izquierda
+
+            GL.End();
+
+            // Restaura el modo de relleno
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+        }
+    }
+}
