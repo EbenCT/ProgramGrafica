90c3706895fe17dcfb7c61388ece3ca047240fea
EbenCT <cayoeben64@gmail.com>
2024-04-26 21:45:52 -0300

    tarea3

--- ConsoleApp1/Modelos3D.cs
+++ ConsoleApp1/Modelos3D.cs
@@ -1,86 +1,22 @@
-﻿using OpenTK;
+﻿using ConsoleApp1.Tarea3;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenTK.Graphics.OpenGL;
-using System.Drawing;
-using ConsoleApp1.Tarea3;
-using Newtonsoft.Json;
 
 namespace ConsoleApp1
 {
-    public class Game : GameWindow
+    internal class Modelos3D
     {
-        private Escenario Escenario1;
-        private float rotationAngle = 0.0f;
-
-        public Game() : base(800, 600) // Constructor que define el tamaño de la ventana
+        //Vertices de la television
+        public static List<Cara> getTvVertices()
         {
-            Escenario1=new Escenario(0.0f,0.0f,0.0f);
-            Objeto tele1=new Objeto(0.0f, 0.0f, 0.0f);
-            Objeto florero1 = new Objeto(0.0f, 4.0f, -0.5f);
-            Objeto parlante1 = new Objeto(-11.0f, -7.0f, +0.5f);
-            Objeto parlante2 = new Objeto(9.0f, -7.0f, +0.5f);
-            //Partes de la tele
-            Parte pantalla = new Parte();
-            Parte cuerpo = new Parte();
-            Parte bases = new Parte();
-            Parte plataforma = new Parte();
-            //Partes del florero
-            Parte tierra = new Parte();
-            Parte maseta = new Parte();
-            Parte flor = new Parte();
-            //Partes del parlante
-            Parte frente = new Parte();
-            Parte caja = new Parte();
-            //Caras
-            Cara frenteTv = new Cara();
-            Cara CuerpoTv = new Cara();
-            Cara BaseTV = new Cara();
-            Cara plataformaTV = new Cara();
-            Cara tierraF = new Cara();
-            Cara macetaF = new Cara();
-            Cara florF = new Cara();
-            Cara bocinas = new Cara();
-            Cara cuerpoP = new Cara();
+            List<Cara> carasTv = new List<Cara>();
 
-            //Armar el escenario
-            Escenario1.AgregarObjeto("Tele1", tele1);
-            Escenario1.AgregarObjeto("Florero1", florero1);
-            Escenario1.AgregarObjeto("Parlante1", parlante1);
-            Escenario1.AgregarObjeto("Parlante2", parlante2);
-
-            //Ensamblar Tele
-            tele1.AgregarParte("pantalla", pantalla);
-            tele1.AgregarParte("cuerpo", cuerpo);
-            tele1.AgregarParte("base", bases);
-            tele1.AgregarParte("plataforma", plataforma);
-
-            //Ensamblar florero
-            florero1.AgregarParte("tierra", tierra);
-            florero1.AgregarParte("maseta", maseta);
-            florero1.AgregarParte("flor", flor);
-
-            //Ensamblar parlante
-            parlante1.AgregarParte("bocinas", frente);
-            parlante1.AgregarParte("cuerpo", caja);
-            parlante2.AgregarParte("bocinas", frente);
-            parlante2.AgregarParte("cuerpo", caja);
-
-            //Ensamblar partes
-            pantalla.AgregarCara("frente", frenteTv);
-            cuerpo.AgregarCara("cuerpo", CuerpoTv);
-            bases.AgregarCara("base", BaseTV);
-            plataforma.AgregarCara("plataforma", plataformaTV);
-            tierra.AgregarCara("tierra", tierraF);
-            maseta.AgregarCara("maseta", macetaF);
-            flor.AgregarCara("flor", florF);
-            frente.AgregarCara("frente", bocinas);
-            caja.AgregarCara("cuerpo", cuerpoP);
-            
-            //Vertices
+            Cara pantalla=new Cara();
             Vertice[] v1 = {
                 new Vertice(- 5.5, - 3.0, 0.0),
                 new Vertice(5.5, -3, 0),
@@ -91,8 +27,7 @@ namespace ConsoleApp1
                 new Vertice(- 5.5, - 3.0, 0.0),
                 new Vertice(-5.5, 3.5, 0),
             };
-            frenteTv.LoadVertices(v1);
-
+            Cara Cuerpo = new Cara();
             Vertice[] v2 = {
                 new Vertice(-6, -5, 0),
                 new Vertice(6, -5, 0),
@@ -135,8 +70,7 @@ namespace ConsoleApp1
                 new Vertice(6, -5, 0),
 
             };
-            CuerpoTv.LoadVertices(v2);
-
+            Cara Base = new Cara();
             Vertice[] v3 = {
                 new Vertice(-1, -5, -0.75),
                 new Vertice(1, -5, -0.75),
@@ -167,9 +101,9 @@ namespace ConsoleApp1
                 new Vertice(1, -6, -0.75),
                 new Vertice(1, -5, -0.75)
             };
-            BaseTV.LoadVertices(v3);
-
-            Vertice[] v4 ={
+            Cara plataforma = new Cara();
+            Vertice[] v4 =
+            {
                 new Vertice(-4, -6, 2), // Esquina inferior izquierda
                 new Vertice(4, -6, 2),
                 new Vertice(4, -6, 2), // Esquina inferior derecha
@@ -179,32 +113,25 @@ namespace ConsoleApp1
                 new Vertice(-4, -6, 2),
                 new Vertice(-4, -6, -2),// Esquina superior izquierda
             };
-            plataformaTV.LoadVertices(v4);
 
-            Vertice[] f1 =
+            pantalla.LoadVertices(v1);
+            Cuerpo.LoadVertices(v2);
+            Base.LoadVertices(v3);
+            plataforma.LoadVertices(v4);
+            carasTv.Add(pantalla);
+            carasTv.Add(Cuerpo);
+            carasTv.Add(Base);
+            carasTv.Add(plataforma);
+            
+            return carasTv;
+        }
+        public static List<Cara> getFloreroVertices()
+        {
+            List<Cara> carasFlorero = new List<Cara>();
+
+            Cara Florero = new Cara();
+            Vertice[] v1 =
             {
-                new Vertice(-0.75f, 2f, 0f),    // A
-                new Vertice(-0.375f, 2f, -0.866f),  // B
-
-                new Vertice(-0.375f, 2f, -0.866f),  // B
-                new Vertice(0.375f, 2f, -0.866f),   // C
-
-                new Vertice(0.375f, 2f, -0.866f),   // C
-                new Vertice(0.75f, 2f, 0f),    // D
-                new Vertice(0.375f, 2f, 0.866f),    // E
-
-                new Vertice(0.75f, 2f, 0f),    // D
-                new Vertice(0.375f, 2f, 0.866f),    // E
-
-                new Vertice(-0.375f, 2f, 0.866f),   // F
-
-                new Vertice(-0.375f, 2f, 0.866f),   // F
-                new Vertice(-0.75f, 2f, 0f),    // A
-            };
-            tierraF.LoadVertices(f1);
-
-            Vertice[] f2 =
-{
                 //base
                 new Vertice(-0.5f, 0f, 0f),
                 new Vertice(-0.25f, 0f, -0.5f),
@@ -262,9 +189,29 @@ namespace ConsoleApp1
                 new Vertice(-1f, 2f, 0f),
                 new Vertice(-0.5f, 2f, 1f),
             };
-            macetaF.LoadVertices(f2);
+            Cara tierra= new Cara();
+            Vertice[] v2 =
+            {
+                new Vertice(-0.75f, 2f, 0f),    // A
+                new Vertice(-0.375f, 2f, -0.866f),  // B
 
-            Vertice[] f3 =
+                new Vertice(-0.375f, 2f, -0.866f),  // B
+                new Vertice(0.375f, 2f, -0.866f),   // C
+
+                new Vertice(0.375f, 2f, -0.866f),   // C
+                new Vertice(0.75f, 2f, 0f),    // D
+                new Vertice(0.375f, 2f, 0.866f),    // E
+
+                new Vertice(0.75f, 2f, 0f),    // D
+                new Vertice(0.375f, 2f, 0.866f),    // E
+
+                new Vertice(-0.375f, 2f, 0.866f),   // F
+
+                new Vertice(-0.375f, 2f, 0.866f),   // F
+                new Vertice(-0.75f, 2f, 0f),    // A
+            };
+            Cara flor = new Cara();
+            Vertice[] v3 =
             {
                 //Hoja1
                 new Vertice(0.5f, 2f, 0f),    // A
@@ -397,9 +344,23 @@ namespace ConsoleApp1
                 new Vertice(-0.05f, 5f, 0.1f),   // F
                 new Vertice(-0.15f, 5f, 0f),    // A
             };
-            florF.LoadVertices(f3);
+            tierra.LoadVertices(v2);
+            Florero.LoadVertices(v1);
+            flor.LoadVertices(v3);
 
-            Vertice[] p1 =
+            carasFlorero.Add(tierra);
+            carasFlorero.Add(Florero);
+            carasFlorero.Add(flor);
+
+            return carasFlorero;
+        }
+
+        public static List<Cara> getParlanteVertices()
+        {
+            List<Cara> carasParlante = new List<Cara>();
+
+            Cara frente = new Cara();
+            Vertice[] v1 =
             {
                 new Vertice(0.0,0.0,0.0),
                 new Vertice(0.0,7.0,0.0),
@@ -475,9 +436,8 @@ namespace ConsoleApp1
                 new Vertice(1.2f, 2.5f, -0.2f)     // F
 
             };
-            bocinas.LoadVertices(p1);
-
-            Vertice[] p2 =
+            Cara cuerpo = new Cara();
+            Vertice[] v2 =
             {
                 new Vertice(0.0, 0.0, -2.0),
                 new Vertice(0.0,0.0,0.0),
@@ -504,67 +464,13 @@ namespace ConsoleApp1
                 new Vertice(3.0, 7.0, -2.0),
                 new Vertice(0.0, 7.0, -2.0)
             };
-            cuerpoP.LoadVertices(p2);
-            //Intento de serealizacion
-            string output = JsonConvert.SerializeObject(Escenario1);
-            Console.WriteLine(output);
-        }
 
-        private void Windows_KeyDown(object sender, OpenTK.Input.KeyboardKeyEventArgs e)
-        {
-            // Rotar la vista en función de las teclas de flecha presionadas
-            switch (e.Key)
-            {
-                case OpenTK.Input.Key.Left:
-                    rotationAngle -= 5.0f; // Girar hacia la izquierda
-                    break;
-                case OpenTK.Input.Key.Right:
-                    rotationAngle += 5.0f; // Girar hacia la derecha
-                    break;
-            }
-        }
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
+            frente.LoadVertices(v1);
+            cuerpo.LoadVertices(v2);
+            carasParlante.Add(frente);
+            carasParlante.Add(cuerpo);
 
-            GL.ClearColor(Color.FromArgb(135, 206, 235));
-            GL.Enable(EnableCap.DepthTest); // Profundidad
-        }
-
-        protected override void OnUpdateFrame(FrameEventArgs e)
-        {
-            base.OnUpdateFrame(e);
-
-            rotationAngle += 6.5f * (float)e.Time; // Rotación
-        }
-
-        protected override void OnRenderFrame(FrameEventArgs e)
-        {
-            base.OnRenderFrame(e);
-
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadIdentity();
-            GL.Frustum(-1.0, 1.0, -1.0, 1.0, 1.0, 100.0); // Proyección perspectiva
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadIdentity();
-
-            // Posiciona y orienta la cámara
-            GL.Translate(0.0f, 0.0f, -15.0f); // Mueve la cámara hacia atrás
-            GL.Rotate(20.0f, 1.0f, 0.0f, 0.0f); // Inclina la vista hacia abajo
-            GL.Rotate(40.0f + rotationAngle, 0.0f, 1.0f, 0.0f); // Rota la vista hacia un ángulo lateral
-
-            Escenario1.Dibujar();
-
-            SwapBuffers();
-        }
-
-        protected override void OnResize(EventArgs e)
-        {
-            base.OnResize(e);
-
-            // Ajustar la vista en caso de que se redimensione la ventana
-            GL.Viewport(0, 0, Width, Height);
+            return carasParlante;
         }
     }
 }
---
+++ ConsoleApp1/florero.cs
@@ -0,0 +1,96 @@
+﻿using OpenTK.Graphics.OpenGL; // Importa la clase GL desde la biblioteca OpenTK.Graphics.OpenGL
+using System; // Importa el espacio de nombres System
+
+namespace ConsoleApp1 // Define un nuevo espacio de nombres ConsoleApp1
+{
+    internal class FlowerPot3D // Define una clase interna llamada FlowerPot3D
+    {
+        private float originX, originY, originZ; // Declara variables para almacenar las coordenadas de origen del florero
+        private float radius, height; // Declara variables para almacenar el radio y la altura del florero
+
+        // Constructor de la clase FlowerPot3D que inicializa las coordenadas de origen, el radio y la altura del florero
+        public FlowerPot3D(float originX, float originY, float originZ, float radius, float height)
+        {
+            this.originX = originX; // Asigna el valor de originX al atributo originX de la clase
+            this.originY = originY; // Asigna el valor de originY al atributo originY de la clase
+            this.originZ = originZ; // Asigna el valor de originZ al atributo originZ de la clase
+            this.radius = radius; // Asigna el valor de radius al atributo radius de la clase
+            this.height = height; // Asigna el valor de height al atributo height de la clase
+        }
+
+        // Método para dibujar el florero en 3D
+        public void Draw()
+        {
+            GL.Begin(PrimitiveType.Quads); // Inicia el dibujo de un grupo de cuadriláteros
+
+            // Base del florero
+            GL.Color3(0.5f, 0.35f, 0.05f); // Establece el color de la base del florero como marrón
+            DrawCylinder(originX, originY, originZ, radius, height); // Dibuja el cilindro que representa la base del florero
+
+            // Tierra en el florero
+            GL.Color3(0.2f, 0.8f, 0.2f); // Establece el color de la tierra en el florero como verde claro
+            DrawDisk(originX, originY, originZ, radius); // Dibuja el disco que representa la tierra en el florero
+
+            // Círculo sobre la plana
+            float circleCenterX = (originX + originX + 6) / 2; // Centra el círculo en el plano horizontal
+            float circleCenterY = originY + 4;
+            float circleRadius = 6f; // Diámetro del círculo igual al ancho de la figura
+            DrawDisk(circleCenterX, circleCenterY, originZ - 1, circleRadius);
+
+            GL.End(); // Finaliza el dibujo de los cuadriláteros
+        }
+
+        // Método privado para dibujar un cilindro en 3D
+        private void DrawCylinder(float x, float y, float z, float radius, float height)
+        {
+            int sides = 20; // Número de lados para la aproximación del cilindro
+
+            // Calcula la mitad de la altura para posicionar el cilindro horizontalmente
+            float halfHeight = height / 2;
+
+            // Itera sobre los lados del cilindro
+            for (int i = 0; i < sides; i++)
+            {
+                float angle0 = (float)(i * 2 * Math.PI / sides); // Calcula el ángulo inicial del lado actual
+                float angle1 = (float)((i + 1) * 2 * Math.PI / sides); // Calcula el ángulo final del lado actual
+
+                // Calcula las coordenadas de los puntos en el lado actual del cilindro
+                float x0 = x + radius * (float)Math.Cos(angle0);
+                float y0 = y + radius * (float)Math.Sin(angle0);
+                float x1 = x + radius * (float)Math.Cos(angle1);
+                float y1 = y + radius * (float)Math.Sin(angle1);
+
+                // Dibuja el lado del cilindro horizontalmente
+                GL.Vertex3(x0, y0, z - halfHeight); // Resta la mitad de la altura para mantener el cilindro horizontal
+                GL.Vertex3(x1, y1, z - halfHeight); // Resta la mitad de la altura para mantener el cilindro horizontal
+                GL.Vertex3(x1, y1, z + halfHeight); // Suma la mitad de la altura para mantener el cilindro horizontal
+                GL.Vertex3(x0, y0, z + halfHeight); // Suma la mitad de la altura para mantener el cilindro horizontal
+            }
+        }
+
+
+        // Método privado para dibujar un disco en 3D
+        private void DrawDisk(float x, float y, float z, float radius)
+        {
+            int sides = 20; // Número de lados para la aproximación del disco
+
+            // Itera sobre los lados del disco
+            for (int i = 0; i < sides; i++)
+            {
+                float angle0 = (float)(i * 2 * Math.PI / sides); // Calcula el ángulo inicial del lado actual del disco
+                float angle1 = (float)((i + 1) * 2 * Math.PI / sides); // Calcula el ángulo final del lado actual del disco
+
+                // Calcula las coordenadas de los puntos en el lado actual del disco
+                float x0 = x + radius * (float)Math.Cos(angle0);
+                float y0 = y + radius * (float)Math.Sin(angle0);
+                float x1 = x + radius * (float)Math.Cos(angle1);
+                float y1 = y + radius * (float)Math.Sin(angle1);
+
+                // Dibuja el lado del disco
+                GL.Vertex3(x0, y0, z);
+                GL.Vertex3(x1, y1, z);
+                GL.Vertex3(x, y, z);
+            }
+        }
+    }
+}
